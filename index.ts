97b5dc1e3c4895ebb8dc2b5018438cc2304cc321
HeenriqueCDS <heenriquecds@gmail.com>
2024-01-09 15:51:57 -0300

    fix: delete index.ts

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { Handler, SQSEvent } from 'aws-lambda'
-
-export const handler: Handler = (event: SQSEvent) => {
-  event.Records.forEach((record) => {
-    const { body } = record
-    console.log(body)
-  })
-}
