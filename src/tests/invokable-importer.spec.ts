a5b9799d0970b58fdc91b29066b8862291bfd918
HeenriqueCDS <heenriquecds@gmail.com>
2024-01-10 15:06:59 -0300

    fix: use hasTable instead createIfNotExists

--- src/tests/invokable-importer.spec.ts
+++ src/tests/invokable-importer.spec.ts
@@ -5,6 +5,22 @@ import { Callback, Context, SQSEvent } from 'aws-lambda'
 let context: Context
 let callback: Callback
 
+const eventMock = {
+  messageId: '1',
+  receiptHandle: '2',
+  awsRegion: 'us-east-1',
+  eventSource: 'aws:sqs',
+  eventSourceARN: 'arn:aws:sqs:us-east-1:123456789012:my-queue',
+  md5OfBody: '7b270e59b47ff90a553787216d55d91d',
+  messageAttributes: {},
+  attributes: {
+    ApproximateReceiveCount: '1',
+    SentTimestamp: '1545082649183',
+    SenderId: '123456789012',
+    ApproximateFirstReceiveTimestamp: '1545082649185',
+  },
+}
+
 describe('invokable-importer', () => {
   afterAll(async () => {
     await db('history').delete()
@@ -12,24 +28,12 @@ describe('invokable-importer', () => {
     await db.destroy()
   })
 
-  it('should be able to import ticker and hsitory', async () => {
+  it('should be able to import ticker and history', async () => {
     const event: SQSEvent = {
       Records: [
         {
-          messageId: '1',
-          receiptHandle: '2',
-          awsRegion: 'us-east-1',
           body: JSON.stringify({ tickerId: 'PETR4' }),
-          eventSource: 'aws:sqs',
-          eventSourceARN: 'arn:aws:sqs:us-east-1:123456789012:my-queue',
-          md5OfBody: '7b270e59b47ff90a553787216d55d91d',
-          messageAttributes: {},
-          attributes: {
-            ApproximateReceiveCount: '1',
-            SentTimestamp: '1545082649183',
-            SenderId: '123456789012',
-            ApproximateFirstReceiveTimestamp: '1545082649185',
-          },
+          ...eventMock,
         },
       ],
     }
@@ -41,5 +45,19 @@ describe('invokable-importer', () => {
 
     const history = await db('history').where('quoteSymbol', 'PETR4')
     expect(history).toHaveLength(61)
+  })
+
+  it('should throw an error if ticker is not found', async () => {
+    const event: SQSEvent = {
+      Records: [
+        {
+          body: JSON.stringify({ tickerId: 'TICKER ZOADOOOOOOO' }),
+          ...eventMock,
+        },
+      ],
+    }
+    expect(async () => {
+      await handler(event, context, callback)
+    }).rejects.toThrow()
   })
 })
