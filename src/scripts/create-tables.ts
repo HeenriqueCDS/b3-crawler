a7cf52cfb07cbe2bcf728bbbd97283737a6a4509
HeenriqueCDS <heenriquecds@gmail.com>
2024-01-10 15:07:10 -0300

    fix: use hasTable instead createIfNotExists

--- src/scripts/create-tables.ts
+++ src/scripts/create-tables.ts
@@ -1,37 +1,46 @@
 import { db } from '@/services/database'
 
 const createTables = async () => {
-  await db.schema.createTableIfNotExists('quote', (table) => {
-    table.string('symbol').primary()
-    table.string('currency')
-    table.string('shortName')
-    table.string('longName')
-    table.float('regularMarketPrice')
-    table.float('regularMarketChange')
-    table.float('regularMarketChangePercent')
-    table.string('logoUrl')
-    table.date('updatedAt')
-    table.float('fiftyTwoWeekLow')
-    table.float('fiftyTwoWeekHigh')
-    table.float('marketCap')
-    table.float('regularMarketVolume')
-    table.float('regularMarketOpen')
-    table.float('regularMarketDayHigh')
-    table.float('regularMarketDayLow')
-    table.float('regularMarketPreviousClose')
+  await db.schema.hasTable('quote').then(async (exists) => {
+    if (!exists) {
+      await db.schema.createTableIfNotExists('quote', (table) => {
+        table.string('symbol').primary()
+        table.string('currency')
+        table.string('shortName')
+        table.string('longName')
+        table.float('regularMarketPrice')
+        table.float('regularMarketChange')
+        table.float('regularMarketChangePercent')
+        table.string('logoUrl')
+        table.date('updatedAt')
+        table.float('fiftyTwoWeekLow')
+        table.float('fiftyTwoWeekHigh')
+        table.float('marketCap')
+        table.float('regularMarketVolume')
+        table.float('regularMarketOpen')
+        table.float('regularMarketDayHigh')
+        table.float('regularMarketDayLow')
+        table.float('regularMarketPreviousClose')
+      })
+    }
   })
 
   await db.schema
-    .createTableIfNotExists('history', (table) => {
-      table.increments('id').primary()
-      table.string('quoteSymbol').references('symbol').inTable('quote')
-      table.integer('date')
-      table.float('open')
-      table.float('high')
-      table.float('low')
-      table.float('close')
-      table.float('volume')
-      table.float('adjustedClose')
+    .hasTable('history')
+    .then(async (exists) => {
+      if (!exists) {
+        await db.schema.createTable('history', (table) => {
+          table.increments('id').primary()
+          table.string('quoteSymbol').references('symbol').inTable('quote')
+          table.integer('date').unique()
+          table.float('open')
+          table.float('high')
+          table.float('low')
+          table.float('close')
+          table.float('volume')
+          table.float('adjustedClose')
+        })
+      }
     })
     .finally(() => {
       db.destroy()
