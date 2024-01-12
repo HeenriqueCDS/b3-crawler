e61e08d3b0efa1dbdb62990323703287f50b905d
HeenriqueCDS <heenriquecds@gmail.com>
2024-01-12 17:43:03 -0300

    fix: add ssl

--- src/services/database.ts
+++ src/services/database.ts
@@ -5,7 +5,7 @@ const db = knex({
   client: 'pg',
   connection: {
     host: process.env.DATABASE_HOST,
-    ssl: true,
+    ssl: true, // For AWS RDS, deactivate to local database
     port: 5432,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
