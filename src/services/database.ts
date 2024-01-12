47cc5bbd95dd78a84669c49aaaac511fcec0f44f
HeenriqueCDS <heenriquecds@gmail.com>
2024-01-12 17:42:55 -0300

    fix: add ssl

--- src/services/database.ts
+++ src/services/database.ts
@@ -5,10 +5,11 @@ const db = knex({
   client: 'pg',
   connection: {
     host: process.env.DATABASE_HOST,
+    ssl: true,
     port: 5432,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    // database: process.env.DATABASE_NAME, #Optional
   },
 })
 
