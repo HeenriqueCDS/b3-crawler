f257d5aeb888bbf32a25c270a5603e23a4b4f73d
HeenriqueCDS <heenriquecds@gmail.com>
2024-01-30 11:21:14 -0300

    feat: ticker import message

--- src/functions/invocable-importer.ts
+++ src/functions/invocable-importer.ts
@@ -1,4 +1,6 @@
 import { db } from '@/services/database'
+import { pusher } from '@/services/pusher'
+
 import { getCompleteQuote } from '@/utils/client/get-complete-quote'
 import { SQSHandler } from 'aws-lambda'
 
@@ -12,11 +14,16 @@ export const handler: SQSHandler = async (event) => {
 
       const { ticker, history } = await getCompleteQuote(tickerId)
 
+      if (!ticker)
+        pusher.trigger(`${tickerId}-tracker`, 'stock-not-found', tickerId)
+
       await db('quote').insert(ticker).onConflict('symbol').merge()
       await db('history')
         .insert(history)
         .onConflict(['quoteSymbol', 'date'])
         .merge()
+
+      pusher.trigger(`${tickerId}-tracker`, 'stock-updated', ticker)
     }
   } catch (error: any) {
     throw new Error(error)
