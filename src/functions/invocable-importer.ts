0a3fde4dc630c5541c504666573e74e039e01036
HeenriqueCDS <heenriquecds@gmail.com>
2024-02-01 11:39:17 -0300

    fix: error throw

--- src/functions/invocable-importer.ts
+++ src/functions/invocable-importer.ts
@@ -5,27 +5,21 @@ import { getCompleteQuote } from '@/utils/client/get-complete-quote'
 import { SQSHandler } from 'aws-lambda'
 
 export const handler: SQSHandler = async (event) => {
-  try {
-    for (const record of event.Records) {
-      const sqsMessage = JSON.parse(record.body)
-      const tickerId = sqsMessage.tickerId
-
-      console.log(`SQS_IMPORTER: ${tickerId}`)
-
+  for (const record of event.Records) {
+    const sqsMessage = JSON.parse(record.body)
+    const tickerId = sqsMessage.tickerId
+    console.log(`SQS_IMPORTER: ${tickerId}`)
+    try {
       const { ticker, history } = await getCompleteQuote(tickerId)
-
-      if (!ticker)
-        pusher.trigger(`${tickerId}-tracker`, 'stock-not-found', tickerId)
-
       await db('quote').insert(ticker).onConflict('symbol').merge()
       await db('history')
         .insert(history)
         .onConflict(['quoteSymbol', 'date'])
         .merge()
-
       pusher.trigger(`${tickerId}-tracker`, 'stock-updated', ticker)
+    } catch (error) {
+      pusher.trigger(`${tickerId}-tracker`, 'stock-not-found', tickerId)
+      console.error(error)
     }
-  } catch (error: any) {
-    throw new Error(error)
   }
 }
